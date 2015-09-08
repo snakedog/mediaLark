3b7cdb58241524b275519ae1935c246753166d9f
Ruairi O'Reilly <ruairi@snakedogdev.com>
2015-09-08 14:52:08 +0100

    Moved 'fortune cookie functionality into module.'

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -7,16 +8,6 @@ var handlebars = require('express3-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-];
-
-///////
 
 app.set('port', process.env.PORT || 3000);
 
@@ -28,8 +19,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/about', function(req, res){
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune.getFortune()});
 });
 
 // custom 404 page
