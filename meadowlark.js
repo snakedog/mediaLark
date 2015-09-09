d361dffd47c2657a45c48f89b6cc2ec9d51fa924
Ruairi O'Reilly <ruairi@snakedogdev.com>
2015-09-09 09:57:54 +0100

    QA stuff added p47

--- meadowlark.js
+++ meadowlark.js
@@ -11,6 +11,12 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+	req.query.test === '1';
+	next();
+});
+
 // Setup the static resource directory (CSS, images, client-side-js)
 app.use(express.static(__dirname + '/public'));
 
@@ -19,7 +25,20 @@ app.get('/', function(req, res){
 });
 
 app.get('/about', function(req, res){
-    res.render('about', {fortune: fortune.getFortune()});
+    res.render('about', {
+		fortune: fortune.getFortune(), 
+		pageTestScript: '/qa/tests-about.js'
+	} );
+	
+	
+});
+
+app.get('/tours/hood-river', function(req, res){
+res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+res.render('tours/request-group-rate');
 });
 
 // custom 404 page
